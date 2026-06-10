21c31c48968bc16a99b569149f1d3189d6139645
adam <developer@tophatlimited.com>
2026-06-10 17:44:36 -0400

    chore: Remove obsolete CI scripts and add new build workflow

--- scripts/update_flutter_versions.sh
+++ scripts/update_flutter_versions.sh
@@ -1,48 +1,22 @@
 #!/bin/bash
 set -e
 
-# This script fetches the latest version of a the stable and beta flutter channels
-# and edits the .cirrus.yml file with those versions
+# Fetches the latest stable Flutter version and updates the build workflow
 
 releases_json=$(curl -s https://storage.googleapis.com/flutter_infra_release/releases/releases_linux.json)
 
-# This function edits the .cirrus.yml file with the given flutter version for the given docker tag
-edit_cirrus_file_for_tag() {
-    cirrus_file=".cirrus.yml"
-    docker_tag=$1
-    version=$2
+channel_hash=$(echo "$releases_json" | jq -r '.current_release.stable')
+stable_version=$(echo "$releases_json" | jq -r --arg HASH "$channel_hash" \
+    '.releases[] | select(.hash == $HASH).version')
 
-    # env for yq
-    docker_tag=$docker_tag version=$version \
-        yq -i '(.docker_builder.env.matrix[] | select(.DOCKER_TAG == env(docker_tag)) | .FLUTTER_VERSION) = env(version)' $cirrus_file
-}
+if [ -z "$stable_version" ]; then
+    echo "Error: could not fetch latest stable Flutter version"
+    exit 1
+fi
 
-# This function fetches the latest version of a particular channel (stable, beta) for Flutter
-get_latest_version_in_channel() {
-    channel=$1
-    # This contains the hash of the latest version in the channel
-    channel_hash=$(echo "$releases_json" | jq -r '.current_release.'"$channel")
-    # Look for the version corresponding to the hash in the list of releases
-    version=$(echo "$releases_json" | jq -r --arg HASH "$channel_hash" \
-        '.releases[] | select(.hash == $HASH).version')
-
-    # check not empty
-    if [ -z "$version" ]; then
-        echo "Error fetching latest version in channel $channel"
-        exit 1
-    fi
-
-    echo "$version"
-}
-
-stable_version=$(get_latest_version_in_channel "stable")
-beta_version=$(get_latest_version_in_channel "beta")
-
-echo "Latest beta version: $beta_version"
 echo "Latest stable version: $stable_version"
 
-edit_cirrus_file_for_tag "stable" "$stable_version"
-edit_cirrus_file_for_tag "latest" "$stable_version"
-edit_cirrus_file_for_tag "beta" "$beta_version"
+stable_version=$stable_version \
+    yq -i '.env.FLUTTER_VERSION = env(stable_version)' .github/workflows/build.yml
 
 exit 0
